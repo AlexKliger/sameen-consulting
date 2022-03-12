24e421c11721762a863386d11ead8b5d652e6b07
Alex <alex.kliger@gmail.com>
2022-03-11 20:19:48 -0500

    Improved handling of animations.

--- intersectionObserver.js
+++ intersectionObserver.js
@@ -5,38 +5,23 @@ window.addEventListener("load", (event) => {
 function createIntersectionObserver() {
     let options = {
         root: null,
-        rootMagin: "0px",
-        threshold: "25%"
+        rootMagin: "20px",
+        threshold: "0"
     };
     const observer = new IntersectionObserver(handleIntersection);
 
-    // Observe icon cards.
-    iconCards = document.querySelectorAll(".icon-card");
-    iconCards.forEach(target => {
-        observer.observe(target);
-    });
-    // Observe all p elements.
-    listOfP = document.querySelectorAll("p");
-    listOfP.forEach(p => {
-        observer.observe(p);
-    });
-
-    sidebar = document.querySelector(".sidebar");
-    observer.observe(sidebar);
+    // All elements that are to be animated must have the animated class as a hook for JS.
+    animatedElements = document.querySelectorAll(".animated");
+    animatedElements.forEach(elem => {
+        observer.observe(elem);
+    })
 }
 
 function handleIntersection(entries, observer) {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            element = entry.target;
-            // Element comes into view, so add respective animation classes.
-            if (element.classList.contains("icon-card")) {
-                element.classList.add("animation-slide-from-right");
-            } else if (element.classList.contains("sidebar")) {
-                element.classList.add("animation-slide-from-left")
-            } else if (element.nodeName == "P") {
-                element.classList.add("animation-slide-from-bottom");
-            }
+            entry.target.classList.remove("animated")
+            entry.target.classList.add("animate")
         }
     });
 }
