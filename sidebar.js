ee4be8cabe862ae3e01fc86cbfb4324e558603b6
Alex <alex.kliger@gmail.com>
2022-03-16 13:39:06 -0400

    Improved responsiveness of about section details.

--- sidebar.js
+++ sidebar.js
@@ -1,12 +1,10 @@
 function toggleSidebar() {
-    sidebar = document.getElementsByClassName("l-sidebar")[0];
+    sidebar = document.getElementsByClassName("l-sidebar")[0].getElementsByTagName("header")[0];
     opaqueOverlay = document.getElementsByClassName("opaque-overlay")[0];
     if (!sidebar.classList.contains("is-responsive")) {
-        console.log("sidebar off");
         sidebar.classList.add("is-responsive");
         opaqueOverlay.style.display = "none";
     } else {
-        console.log("sidebar on");
         sidebar.classList.remove("is-responsive");
         opaqueOverlay.style.display = "block";
     }
